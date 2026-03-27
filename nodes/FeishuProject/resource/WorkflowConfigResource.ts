686e1ba9e8967ea75147754b0bb2d41c4cc4a46f
luka-mimi <luka.cat.mimi@gmail.com>
2026-03-27 09:38:03 +0800

    fix: 修改缺失的名字

--- nodes/FeishuProject/resource/WorkflowConfigResource.ts
+++ nodes/FeishuProject/resource/WorkflowConfigResource.ts
@@ -1,7 +1,7 @@
 import { ResourceOptions } from '../../help/type/IResource';
 
 const WorkflowConfigResource: ResourceOptions = {
-	name: '程模板配置',
+	name: '流程模板配置',
 	value: 'workflow_config',
 	order: 50,
 };
