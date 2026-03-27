24f51f2648d0784f4fd07637e48e311f527e209e
luka-mimi <luka.cat.mimi@gmail.com>
2026-03-27 10:29:55 +0800

    feat: add new options for WBS view operation and remove commented-out field type options

--- nodes/FeishuProject/resource/workflow_node/WorkflowNodeWbsViewOperate.ts
+++ nodes/FeishuProject/resource/workflow_node/WorkflowNodeWbsViewOperate.ts
@@ -1,8 +1,15 @@
 import { IDataObject, IExecuteFunctions } from 'n8n-workflow';
 import RequestUtils from '../../../help/utils/RequestUtils';
 import { ResourceOperations } from '../../../help/type/IResource';
-import { commonOptions, ICommonOptionsValue } from '../../../help/utils/sharedOptions';
+import { batchingOption, timeoutOption, ICommonOptionsValue } from '../../../help/utils/sharedOptions';
 import { DESCRIPTIONS } from '../../../help/description';
+
+interface IWbsViewOptions extends ICommonOptionsValue {
+	need_union_deliverable?: boolean;
+	need_schedule_table_agg?: boolean;
+	need_wbs_relation_chain_path?: boolean;
+	need_wbs_relation_chain_entity?: boolean;
+}
 
 const WorkflowNodeWbsViewOperate: ResourceOperations = {
 	name: '获取工作流详情（WBS）',
@@ -29,7 +36,45 @@ const WorkflowNodeWbsViewOperate: ResourceOperations = {
 			default: '',
 			description: '工作项实例 ID，在工作项实例详情中，展开右上角 ··· > ID 获取。',
 		},
-		commonOptions,
+		{
+			displayName: 'Options',
+			name: 'options',
+			type: 'collection',
+			placeholder: 'Add option',
+			default: {},
+			options: [
+				{
+					displayName: '是否融合需要交付物',
+					name: 'need_union_deliverable',
+					type: 'boolean',
+					default: false,
+					description: 'Whether to include merged deliverable information. 是否需要融合交付物信息。',
+				},
+				{
+					displayName: '是否需要计划表聚合字段',
+					name: 'need_schedule_table_agg',
+					type: 'boolean',
+					default: false,
+					description: 'Whether to include schedule table custom column aggregation fields. 扩展查询，是否需要计划表自定义列聚合字段。',
+				},
+				{
+					displayName: '是否需要WBS链路层级信息',
+					name: 'need_wbs_relation_chain_path',
+					type: 'boolean',
+					default: false,
+					description: 'Whether to include WBS relation chain path information. 扩展查询，是否需要WBS链路层级信息。',
+				},
+				{
+					displayName: '是否需要WBS链路实例信息',
+					name: 'need_wbs_relation_chain_entity',
+					type: 'boolean',
+					default: false,
+					description: 'Whether to include WBS relation chain entity information. 扩展查询，是否需要WBS链路实例信息。',
+				},
+				batchingOption,
+				timeoutOption,
+			],
+		},
 	],
 	async call(this: IExecuteFunctions, index: number): Promise<IDataObject> {
 		const project_key = this.getNodeParameter('project_key', index, '', {
@@ -37,11 +82,38 @@ const WorkflowNodeWbsViewOperate: ResourceOperations = {
 		}) as string;
 		const work_item_type_key = this.getNodeParameter('work_item_type_key', index) as string;
 		const work_item_id = this.getNodeParameter('work_item_id', index) as string;
-		const options = this.getNodeParameter('options', index, {}) as ICommonOptionsValue;
+		const options = this.getNodeParameter('options', index, {}) as IWbsViewOptions;
+
+		const qs: IDataObject = {};
+		if (options.need_union_deliverable !== undefined) {
+			qs.need_union_deliverable = options.need_union_deliverable;
+		}
+		if (options.need_schedule_table_agg !== undefined) {
+			qs.need_schedule_table_agg = options.need_schedule_table_agg;
+		}
+
+		const expand: IDataObject = {};
+		if (options.need_union_deliverable !== undefined) {
+			expand.need_union_deliverable = options.need_union_deliverable;
+		}
+		if (options.need_wbs_relation_chain_path !== undefined) {
+			expand.need_wbs_relation_chain_path = options.need_wbs_relation_chain_path;
+		}
+		if (options.need_wbs_relation_chain_entity !== undefined) {
+			expand.need_wbs_relation_chain_entity = options.need_wbs_relation_chain_entity;
+		}
+
+		const body: IDataObject = {};
+		if (Object.keys(expand).length > 0) {
+			body.expand = expand;
+			Object.assign(body, expand);
+		}
 
 		return RequestUtils.request.call(this, {
 			method: 'GET',
 			url: `/open_api/${project_key}/work_item/${work_item_type_key}/${work_item_id}/wbs_view`,
+			...(Object.keys(qs).length > 0 && { qs }),
+			...(Object.keys(body).length > 0 && { body }),
 			timeout: options.timeout,
 		});
 	}
